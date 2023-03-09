df4434c3707228a763cfeebc444b1823c5e41bc7
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-03-09 18:16:14 +0100

    Add project files.

---
+++ Semestr1_Lekcja1_2023_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Semestr1_Lekcja1_2023_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Wypisywanie tekstu
+            Console.WriteLine("Witaj w Gigantach");
+
+            // Zatrzymanie konsoli
+            Console.ReadLine();
+
+        }
+    }
+}
